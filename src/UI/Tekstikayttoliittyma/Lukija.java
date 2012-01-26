190f9a90da01b3d5671517715e4cc9453364663a
Jarmo <jsi1992@gmail.com>
2012-01-26 13:01:15 +0200

    testejä

--- src/UI/Tekstikayttoliittyma/Lukija.java
+++ src/UI/Tekstikayttoliittyma/Lukija.java
@@ -11,23 +11,29 @@ public class Lukija {
 
     /**
      * Näytetään viesti ja kysytään String merkkijono.
-     * 
+     *
      * @param viesti
-     * @return 
+     *
+     * @return
      */
     public String kysyString(String viesti) {
         System.out.println(viesti);
         return l.nextLine();
     }
-    
+
     /**
      * Näytetään viesti ja kysytään int luku.
-     * 
+     *
      * @param viesti
-     * @return 
+     *
+     * @return
      */
     public int kysyInt(String viesti) {
-        System.out.println(viesti);
-        return Integer.parseInt(l.nextLine());
+        try {
+            System.out.println(viesti);
+            return Integer.parseInt(l.nextLine());
+        } catch (Exception e) {
+            return -1;
+        }
     }
 }
---
+++ test/UI/Tekstikayttoliittyma/LukijaTest.java
@@ -0,0 +1,42 @@
+package UI.Tekstikayttoliittyma;
+
+import fi.helsinki.cs.tmc.edutestutils.MockStdio;
+import static org.junit.Assert.assertEquals;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ *
+ * @author jarmo
+ */
+public class LukijaTest {
+
+    @Rule
+    public MockStdio io = new MockStdio();
+
+    @Test
+    public void testKysyString() {
+        String merkkijono = "tämä on ilkeä testi trolololoololololoooo";
+        io.setSysIn(merkkijono + "\n");
+        Lukija l = new Lukija();
+        String saatu = l.kysyString("Anna viesti: ");
+        assertEquals("Ei lue annettua kirjainjokukkoa  oikein.", merkkijono, saatu);
+    }
+
+    @Test
+    public void testKysyInt() {
+        io.setSysIn("5\n");
+        Lukija l = new Lukija();
+        int saatu = l.kysyInt("Anna numero: ");
+        assertEquals("Ei lue annettua int arvoa oikein.", 5, saatu);
+    }
+
+    @Test
+    public void testKysyIntToimiiOikeinVirheTilanteissa() {
+        String merkkijono = "5 trololololoo";
+        io.setSysIn("\n");
+        Lukija l = new Lukija();
+        int saatu = l.kysyInt("Anna numero: ");
+        assertEquals("Virheellisen int arvon antaminen ai toimi, kuuluisi palauttaa \"-1\" ", -1, saatu);
+    }
+}
